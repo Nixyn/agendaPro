6a76a02060d75a56da6b7e2331cddfcbe881ee0e
= <=>
2017-12-04 12:35:37 +0000

    Creada funcionalidad de añadir usuario

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -7,20 +7,41 @@
 
     HomeController.$inject = [];
     function HomeController() {
+        // Variables Declaration
         var vm = this;
         vm.users=[
             {
-                id: "su madre",
+                id: "s58k9qgzqe",
                 photo: "",
                 name: "pedro",
                 phone: "656656565"
             }
         ];
+        vm.newUser = {};
+
+        // Functions Declaration
+        vm.addUser = addUser;
 
         activate();
 
-        ////////////////
+        //////////////// MAIN FUNCTIONS
 
         function activate() { }
+
+        function addUser(){
+            vm.newUser.id = createID();
+            vm.users.push(vm.newUser);
+            cleanFields()
+        }
+
+        //////////////// AUX FUNCTIONS
+
+        function createID() {   
+            return Math.random().toString(36).substr(2, 10);
+        }
+
+        function cleanFields(){
+            vm.newUser = {};
+        }
     }
 })();
